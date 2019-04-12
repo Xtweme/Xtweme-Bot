5d027b939112f30fc7a6474151dc98e263453712
Xtweme <49501074+Xtweme@users.noreply.github.com>
2019-04-11 22:21:50 -0400

    Update ModBot.js

--- ModBot.js
+++ ModBot.js
@@ -115,7 +115,7 @@ client.on("message", async message => {
     // This command removes all messages from all users in the channel, up to 100.
     
     // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
+    const deleteCount = parseInt(args[0]+1, 10);
     if(!message.member.roles.some(r=>["Admin", "Superior Human"].includes(r.name)) )
 
     // Ooooh nice, combined conditions. <3
